83301190948c32d83167d5c32c8d1668b5c2c246
Bandit <rex978956@gmail.com>
2019-06-01 02:12:11 +0800

    [Fix] position of menu

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
 var last_known_scroll_position = 0;
 var ticking = false;
 
-function doSomething(scroll_pos) {
+function fixposition(scroll_pos) {
     var htmlStyles = getComputedStyle(document.documentElement);
     var init = 90;
     var menu_top = init - scroll_pos;
     var article_top = 75 + scroll_pos;
+    var drop_top = 9.5;
     console.log("scroll_pos = " + scroll_pos);
     console.log("fixed = " + menu_top);
     if (menu_top >= 0) {
         document.documentElement.style.setProperty('--menu-top', menu_top + "px");
+        document.documentElement.style.setProperty('--drop-top', drop_top + "rem");
     } else {
         document.documentElement.style.setProperty('--menu-top', "0px");
+        document.documentElement.style.setProperty('--drop-top', "4rem");
     }
 //	if (article_top <= 150) {
 //		document.documentElement.style.setProperty('--article-top', article_top + "px");
@@ -26,7 +29,7 @@ window.addEventListener('scroll', function (e) {
     last_known_scroll_position = window.scrollY;
     if (!ticking) {
         window.requestAnimationFrame(function () {
-            doSomething(last_known_scroll_position);
+            fixposition(last_known_scroll_position);
             ticking = false;
         });
     }
