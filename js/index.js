cb388808df85d22745f20506298ea0944786cf99
allen0099 <s96016641@gmail.com>
2019-06-06 08:41:13 +0800

    Reformat

--- js/index.js
+++ js/index.js
@@ -7,21 +7,21 @@ function fixposition(scroll_pos) {
     let menu_top = init - scroll_pos;
     const drop_top = parseInt(getComputedStyle(document.querySelector('.menu')).height);
     if (menu_top <= 0) {
-	    menu_top = 0;
-	}
+        menu_top = 0;
+    }
     document.documentElement.style.setProperty('--menu-top', menu_top + "px");
     document.documentElement.style.setProperty('--drop-top', menu_top + drop_top + "px");
 }
 
 window.addEventListener('scroll', function (e) {
-	last_known_scroll_position = window.scrollY;
-	if (!ticking) {
-		window.requestAnimationFrame(function () {
-			fixposition(last_known_scroll_position);
-			ticking = false;
-		});
-	}
-	ticking = true;
+    last_known_scroll_position = window.scrollY;
+    if (!ticking) {
+        window.requestAnimationFrame(function () {
+            fixposition(last_known_scroll_position);
+            ticking = false;
+        });
+    }
+    ticking = true;
 });
 
 //var s = document.getElementsByTagName("h1");
